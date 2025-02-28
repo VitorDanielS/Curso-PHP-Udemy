520684a71ef60136cce81e4c2b89926c5b56b670
VitorDanielS <vitordasadm@gmail.com>
2025-02-27 21:41:18 -0300

    terminando exercicio 51 e fazendo aulas 26 27 28 e 29

--- 11_arrays/25_exercicio_51/index.php
+++ 11_arrays/25_exercicio_51/index.php
@@ -1,15 +1,23 @@
 <?php 
   
-  $arr = [
+  $ranking = [
     'Maria' => 100,
     'Ana' => 200,
     'Vitor' => 150,
-    'Daniel' => 250
+    'Daniel' => 250,
+    'Joana' => 50
   ];
 
-  arsort($arr);
+  arsort($ranking);
 
-  print_r($arr);
+  print_r($ranking);
   echo "<br>";
   
 ?>
+
+<h1>Ranking:</h1>
+<ol>
+  <?php foreach($ranking as $pessoa => $pontuacao): ?>
+    <li><?= $pessoa ?> -> <?= $pontuacao ?> pontos</li>
+  <?php endforeach; ?>
+</ol>
